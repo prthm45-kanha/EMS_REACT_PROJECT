e1cbe3d6b61953706f822888b92f3790da477e21
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-05-23 20:09:35 +0530

    done with admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './component/Auth/Login'
 import EmployeDashboard from './component/Dashboard/EmployeDashboard'
+import AdminDashboard from './component/Dashboard/AdminDashboard'
 
 const App = () => {
   return (
     <div className='bg-black h-screen w-screen'>
     {/* <Login/> */}
-    <EmployeDashboard/>
+    {/* <EmployeDashboard/> */}
+    <AdminDashboard/>
     </div>
   )
 }
